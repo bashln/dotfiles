3ddc99296d052b06f0ba443a4f123091789e7ec0
leonamduarte <lpdmonteiro@gmail.com>
2026-07-08 07:36:05 -0300

    add removendo spellcheck para text, plaintext, typst, gitcommit e markdown no neovim

--- home/.config/nvim/lua/config/autocmds.lua
+++ home/.config/nvim/lua/config/autocmds.lua
@@ -21,3 +21,13 @@ vim.api.nvim_create_autocmd("VimLeavePre", {
     end
   end,
 })
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = false
+  end,
+})
