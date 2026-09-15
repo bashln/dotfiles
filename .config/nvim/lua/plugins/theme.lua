7aa9142da6874b647f95074a08bba81510dd6026
bashln <lpdmonteiro@gmail.com>
2026-09-15 19:34:09 -0300

    chore: unversion runtime logs and sync terminal/shell configs

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,7 +1,1 @@
-if vim.fn.has("win32") == 1 then
-  return require("plugins.colorscheme.doom-one")
-end
-
-local theme_path = vim.fn.stdpath("state") .. "/omarchy/current/theme/neovim.lua"
-return dofile(theme_path)
-
+../../../../.local/state/omarchy/current/theme/neovim.lua
