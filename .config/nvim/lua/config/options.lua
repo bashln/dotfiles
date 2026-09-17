e2ade63e20e31bdfa38bc9ce2b736996df50b56d
leonamduarte <lpdmonteiro@gmail.com>
2026-09-17 11:16:33 -0300

    fix(nvim): use powershell.exe real path for Windows shell

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,10 +4,9 @@ require("config.remote_clipboard").setup()
 vim.opt.relativenumber = false
 vim.g.autoformat = false
 
--- PowerShell Core no Windows
+-- PowerShell no Windows
 if vim.fn.has("win32") == 1 then
-  local localappdata = os.getenv("LOCALAPPDATA")
-  vim.o.shell = localappdata .. "\\Microsoft\\WindowsApps\\pwsh.exe"
+  vim.o.shell = [[C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe]]
   vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
   vim.o.shellredir = '2>&1 | Out-File -Encoding utf8 %s'
   vim.o.shellpipe = '2>&1 | Out-File -Encoding utf8 %s'
