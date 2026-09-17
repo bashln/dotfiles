a00b657779aa44029f6ab01e69342464c4286fc0
leonamduarte <lpdmonteiro@gmail.com>
2026-09-17 10:37:52 -0300

    fix(nvim): use LOCALAPPDATA path for pwsh on Windows

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,7 +6,8 @@ vim.g.autoformat = false
 
 -- PowerShell Core no Windows
 if vim.fn.has("win32") == 1 then
-  vim.o.shell = "pwsh"
+  local localappdata = os.getenv("LOCALAPPDATA")
+  vim.o.shell = localappdata .. "\\Microsoft\\WindowsApps\\pwsh.exe"
   vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
   vim.o.shellredir = '2>&1 | Out-File -Encoding utf8 %s'
   vim.o.shellpipe = '2>&1 | Out-File -Encoding utf8 %s'
