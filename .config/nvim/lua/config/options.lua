cf6ead713f50215cd28acfc6d5f1bb4b94374304
leonamduarte <lpdmonteiro@gmail.com>
2026-09-17 10:22:53 -0300

    feat(nvim): use pwsh as shell on Windows

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,3 +3,12 @@ require("config.remote_clipboard").setup()
 
 vim.opt.relativenumber = false
 vim.g.autoformat = false
+
+-- PowerShell Core no Windows
+if vim.fn.has("win32") == 1 then
+  vim.o.shell = "pwsh"
+  vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+  vim.o.shellredir = '2>&1 | Out-File -Encoding utf8 %s'
+  vim.o.shellpipe = '2>&1 | Out-File -Encoding utf8 %s'
+  vim.o.shellxquote = ""
+end
