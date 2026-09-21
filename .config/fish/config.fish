09543174d3b50e0eaa8a999f2401b2c32924efbd
bashln <lpdmonteiro@gmail.com>
2026-09-21 18:56:01 -0300

    fix(fish): eza icons flag consumes positional arg as value
    --icons takes optional [<WHEN>] value, so 'ls <dir>' was parsed as
    '--icons <dir>' and errored. Use --icons=always.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -81,10 +81,10 @@ if status is-interactive
 
     # --- LS (eza) ---
     if type -q eza
-        alias ls='eza -al --color=always --group-directories-first --icons'
-        alias la='eza -a  --color=always --group-directories-first --icons'
-        alias ll='eza -l  --color=always --group-directories-first --icons'
-        alias lt='eza -aT --color=always --group-directories-first --icons'
+        alias ls='eza -al --color=always --group-directories-first --icons=always'
+        alias la='eza -a  --color=always --group-directories-first --icons=always'
+        alias ll='eza -l  --color=always --group-directories-first --icons=always'
+        alias lt='eza -aT --color=always --group-directories-first --icons=always'
         alias l.='eza -a | grep -e "^\."'
     end
 
