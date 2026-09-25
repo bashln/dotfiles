ca95fb55e953c7f487cbf60bf1c2a6e4f0ad6daa
leonamduarte <lpdmonteiro@gmail.com>
2026-09-25 16:07:45 -0300

    Enhance Dracula colorscheme configuration

--- .config/nvim/lua/plugins/colorscheme/dracula.lua
+++ .config/nvim/lua/plugins/colorscheme/dracula.lua
@@ -1,2 +1,21 @@
-return
-  { "Mofiqul/dracula.nvim" }
+return {
+  "Mofiqul/dracula.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    -- show the '~' characters after the end of buffers
+    show_end_of_buffer = true,
+    -- use transparent background
+    transparent_bg = true,
+    -- set custom lualine background color
+    lualine_bg_color = "#44475a",
+    -- italic styles
+    italic_comment = true,
+    -- overrides / custom colors
+    overrides = {},
+  },
+  config = function(_, opts)
+    local dracula = require("dracula")
+    dracula.setup(opts)
+  end,
+}
